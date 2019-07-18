8d34e939806936848974c7b79d2bce3d22649744
Jesse Allison <jtallison@lsu.edu>
2019-07-17 21:58:24 -0500

    Fixed registration and sorting by session

--- diamondsCluster/public/js/setup.js
+++ diamondsCluster/public/js/setup.js
@@ -1,15 +1,18 @@
 let user = {
-  'name': 'a_user',
-  'id': 1000
+  name: 'a_user',
+  id: 1000,
+  corpus: 'horrortech-corpus',
+  color: '#3c23ff',
+  location: [0.5, 0.5],
+  date: Date.now()
 }
+user.color = getRandomColor();
 
 // var socket = io.connect('127.0.0.1:8000');
 var socket = io.connect(window.location.origin, {
   transports: ['websocket']
 });
 // var socket = io.connect('https://atlab.cct.lsu.edu/', { path: '/causeway/socket.io' });
-user.color = getRandomColor();
-var myLocation = [0.5, 0.5]; // Default centered
 
 // seatMap.addEventListener("click", getClickPosition, false);
 
@@ -26,7 +29,7 @@ function actOnText() {
 
 }
 
-$('.scoreText').click(function (e) {
+$('.scoreText').click(function(e) {
   target = event.target || event.srcElement;
 
   if (target.nodeName.toLowerCase() === "span") {
@@ -47,53 +50,81 @@ $('.scoreText').click(function (e) {
 // Nexus Stuff
 // **********************************************************
 
-
 function registerWithServer() {
-  // Tone.start();
-  // causeSound.triggerPitch();
-  // causeSound.triggerCauseway();
   socket.emit('addme', {
-    name: user.name,
+    name: "controller",
+    sessionName: user.sessionName,
+    corpus: user.corpus,
     color: user.color
   });
-  // document.getElementsByClassName("sd")[0].style.display = 'none';
-  // document.getElementsByClassName("st")[0].style.display = 'block';
 }
+registerWithServer();
 
 // **********************************************************
 // Only for the Overlay/intro page
 
 function registerPoet(corpusName = 'horrortech-corpus') {
-  // let corpusName = 'horrortech-corpus';
   let sessionName = document.forms["poet-form"]["session-name"].value;
   // let corpusName = document.forms["poet-form"]["corpus-name"].value;
   console.log("Session Name: ", sessionName);
   console.log("Corpus Name: ", corpusName);
 
   socket.emit('registerSession', {
-    'name': sessionName,
+    'username': user.name,
+    'sessionName': sessionName,
     'corpus': corpusName,
-    // 'corpus': 'horrortech-corpus',
-    'date': Date.now()
+    'color': user.color,
+    'date': user.date
   });
   localStorage.setItem("sessionName", sessionName);
+  localStorage.setItem("corpus", corpusName);
+  localStorage.setItem("userColor", user.color);
   user.sessionName = sessionName;
+  user.corpus = corpusName;
   return false;
 };
 
 function registerAudience() {
-  let sessionName = document.forms["audience-form"]["session-name"].value;
+  let sessionName = document.forms["audience-form"]["session-name"].value || 'default';
   console.log("Session Name: ", sessionName);
   socket.emit('joinSession', {
-    'name': sessionName,
-    'user': user.socketID
+    'username': user.name,
+    'sessionName': sessionName,
+    'color': user.color,
+    'date': user.date
   });
   localStorage.setItem("sessionName", sessionName);
+  localStorage.setItem("userColor", user.color);
   user.sessionName = sessionName;
   return false;
 };
 
-socket.on('sessions', function (data) {
+function registerTheater() {
+  let sessionName = document.forms["audience-form"]["session-name"].value || 'default';
+  console.log("Session Name: ", sessionName);
+  socket.emit('joinSession', {
+    'username': user.name,
+    'sessionName': sessionName,
+    'color': user.color,
+    'date': user.date
+  });
+  localStorage.setItem("sessionName", sessionName);
+  localStorage.setItem("userColor", user.color);
+  user.sessionName = sessionName;
+  return false;
+};
+
+socket.on('registerComplete', function(data) {
+  console.log("registerComplete: " + data);
+  user.socketID = data.socketID;
+  localStorage.setItem("socketID", data.socketID);
+  if (data.corpus) {
+    user.corpus = data.corpus;
+    localStorage.setItem("corpus", data.corpus);
+  }
+});
+
+socket.on('sessions', function(data) {
   console.log("sessions data: ", data);
   if (data.list) {
     console.log("Session list: ", data.list);
@@ -105,90 +136,16 @@ function getSessions() {
   socket.emit('getSessions', 'please');
 }
 
-socket.on('chat', function (data) {
-  console.log("chat: " + data.greeting);
-  user.socketID = data.socketID;
-  localStorage.setItem("socketID", data.socketID);
-  if (data.corpus) {
-    user.corpus = data.corpus;
-  }
+socket.on('chat', function(data) {
+  console.log("chat: " + data);
 });
 
-socket.on('setSection', function (data) {
-  // console.log(data);
-  console.log("the section is now: " + data.title);
 
-  // if (data.sect == "15") {
-  // 	Tone.start();
-  // 	causeSound.playBrought();
-  // }
-
-  // if(data.title !== undefined){
-  // 	var otherClasses = document.querySelectorAll('.sec');
-
-  // 	for (var i = 0; i < otherClasses.length; i++) {
-  // 	    otherClasses[i].style.display = 'none';
-  // 	}
-
-  // 	document.getElementsByClassName("s"+data.sect)[0].style.display = 'block';
-  // }
-
-});
-
-// function scrollto(element){
-// 	var ele = document.getElementsByClassName("s"+element)[0];
-//     if (ele) {
-//     	window.scrollTo(0, ele.offsetTop);
-//     }
-// }
-
-socket.on('itemback', function (data) {
-  console.log("itemback:", data.phrase);
-  var elements = document.getElementsByClassName("gentext")[0];
-  elements.innerHTML = data.phrase;
-  // elements[0].className += " clicked";
-});
-
-// function clickgo() {
-// 	var elements = document.getElementsByClassName(this.className);
-// 	socket.emit('item', elements[0].className.split(" ")[1]);
-// 	elements[0].className += " clicked";
-// 	elements[0].style.backgroundColor = user.color;
-// }
-
-
-socket.on('audienceEnable', function (data) {
+socket.on('audienceEnable', function(data) {
   console.log('enabled? ', data);
   dSound.audienceEnable(data);
 });
 
-socket.on('playChord', function (data) {
-  dSound.playChord(data.notes, data.duration);
-});
-
-socket.on('sustainChord', function (data) {
-  dSound.playChord(data.notes, data.duration);
-});
-
-socket.on('triggerBeginning', function (data) {
-  dSound.playKepler();
-});
-
-socket.on('nextChord', function (data) {
-  dSound.nextChord();
-});
-
-socket.on('triggerUtopalypse', function (data) {
-  dSound.playUtopalypse();
-});
-
-socket.on('triggerDiamonds', function (data) {
-  dSound.playDiamonds();
-});
-
-socket.on('triggerEnding', function (data) {
-  dSound.playEnding();
-});
 
 
 function getRandomColor() {
@@ -201,10 +158,6 @@ function getRandomColor() {
 }
 
 
-// Add Chords, Progression, feedback, comb filtering, shimmer!
-
-var self = this;
-
 var speakVoice = 'en/en';
 meSpeak.loadConfig("/js/mespeak/mespeak_config.json");
 //meSpeak.loadVoice('js/mespeak/voices/en/en-us.json');
@@ -216,7 +169,7 @@ var dSound = new DiamondSound();
 function createUL(array) {
   var str = '<ul>'
 
-  array.forEach(function (slide) {
+  array.forEach(function(slide) {
     str += '<li>' + slide + '</li>';
   });
 
@@ -227,4 +180,3 @@ function createUL(array) {
     document.getElementById("session-list").innerHTML = str;
   }
 }
-
