3c1334f332e943a676a2fe4bab442a0208ef7c00
Derick Ostrenko <dostrenko@gmail.com>
2019-07-17 20:06:58 -0500

    cleanup and dropdown

--- diamondsCluster/public/js/setup.js
+++ diamondsCluster/public/js/setup.js
@@ -1,112 +1,64 @@
+/*
+This file includes 2 things: 
+
+- Helpful JS functions
+- Websocket message handling
+
+It is referenced by 3 (soon to be 2) files:
+
+- /sessions/audience.html
+- /sessions/theater.html
+- /setup/index.html
+
+*/
+
+/**********************************************
+VARIABLES
+**********************************************/
+
 let user = {
   'name': 'a_user',
   'id': 1000
 }
 
-// var socket = io.connect('127.0.0.1:8000');
+user.color = getRandomColor();
+
+var myLocation = [0.5, 0.5]; // Default centered
+
+var self = this;
+
+var speakVoice = 'en/en';
+
+var dSound = new DiamondSound();
+
 var socket = io.connect(window.location.origin, {
   transports: ['websocket']
 });
-// var socket = io.connect('https://atlab.cct.lsu.edu/', { path: '/causeway/socket.io' });
-user.color = getRandomColor();
-var myLocation = [0.5, 0.5]; // Default centered
 
-// seatMap.addEventListener("click", getClickPosition, false);
+// Add Chords, Progression, feedback, comb filtering, shimmer!
+meSpeak.loadConfig("/js/mespeak/mespeak_config.json");
+//meSpeak.loadVoice('js/mespeak/voices/en/en-us.json');
+meSpeak.loadVoice('/js/mespeak/voices/' + speakVoice + '.json');
+//meSpeak.speak('hello world',{},toneSetup());
 
-// $(function () {
-function actOnText() {
-  var contents = $('.scoreText').text().split(" "),
-    modText = '';
-
-  for (var i = 0; i < contents.length; i++) {
-    modText += '<span>' + contents[i] + '</span> ';
-  }
-
-  $('.scoreText').html(modText);
-
-}
-
-$('.scoreText').click(function (e) {
-  target = event.target || event.srcElement;
-
-  if (target.nodeName.toLowerCase() === "span") {
-    var text = $(e.target).text();
-
-    // console.log("text:",text);
-    // $(e.target).toggleClass('underline');
-    $(e.target)[0].style.backgroundColor = user.color;
-    // console.log($(e.target)[0]);
-    dSound.speak(text);
-    socket.emit('item', text);
-  }
-});
-
-
-// **********************************************************
-// **********************************************************
-// Nexus Stuff
-// **********************************************************
-
-
-function registerWithServer() {
-  // Tone.start();
-  // causeSound.triggerPitch();
-  // causeSound.triggerCauseway();
-  socket.emit('addme', {
-    name: user.name,
-    color: user.color
-  });
-  // document.getElementsByClassName("sd")[0].style.display = 'none';
-  // document.getElementsByClassName("st")[0].style.display = 'block';
-}
-
-// **********************************************************
-// Only for the Overlay/intro page
-
-function registerPoet(corpusName = 'horrortech-corpus') {
-  // let corpusName = 'horrortech-corpus';
-  let sessionName = document.forms["poet-form"]["session-name"].value;
-  // let corpusName = document.forms["poet-form"]["corpus-name"].value;
-  console.log("Session Name: ", sessionName);
-  console.log("Corpus Name: ", corpusName);
-
-  socket.emit('registerSession', {
-    'name': sessionName,
-    'corpus': corpusName,
-    // 'corpus': 'horrortech-corpus',
-    'date': Date.now()
-  });
-  localStorage.setItem("sessionName", sessionName);
-  user.sessionName = sessionName;
-  return false;
-};
-
-function registerAudience() {
-  let sessionName = document.forms["audience-form"]["session-name"].value;
-  console.log("Session Name: ", sessionName);
-  socket.emit('joinSession', {
-    'name': sessionName,
-    'user': user.socketID
-  });
-  localStorage.setItem("sessionName", sessionName);
-  user.sessionName = sessionName;
-  return false;
-};
+/**********************************************
+ WEBSOCKETS
+**********************************************/
 
 socket.on('sessions', function (data) {
-  console.log("sessions data: ", data);
   if (data.list) {
-    console.log("Session list: ", data.list);
-    createUL(data.list);
+    console.log("Session List: ", data.list);
+    // createUL(data.list);
+    createOpts(data.list);
   }
 });
 
-function getSessions() {
-  socket.emit('getSessions', 'please');
-}
+socket.on('setSection', function (data) {
+  console.log("the section is now: " + data.title);
+});
 
 socket.on('chat', function (data) {
-  console.log("chat: " + data.greeting);
+  console.log("chat: ", data.greeting);
   user.socketID = data.socketID;
   localStorage.setItem("socketID", data.socketID);
   if (data.corpus) {
@@ -114,48 +66,11 @@ socket.on('chat', function (data) {
   }
 });
 
-socket.on('setSection', function (data) {
-  // console.log(data);
-  console.log("the section is now: " + data.title);
-
-  // if (data.sect == "15") {
-  // 	Tone.start();
-  // 	causeSound.playBrought();
-  // }
-
-  // if(data.title !== undefined){
-  // 	var otherClasses = document.querySelectorAll('.sec');
-
-  // 	for (var i = 0; i < otherClasses.length; i++) {
-  // 	    otherClasses[i].style.display = 'none';
-  // 	}
-
-  // 	document.getElementsByClassName("s"+data.sect)[0].style.display = 'block';
-  // }
-
-});
-
-// function scrollto(element){
-// 	var ele = document.getElementsByClassName("s"+element)[0];
-//     if (ele) {
-//     	window.scrollTo(0, ele.offsetTop);
-//     }
-// }
-
 socket.on('itemback', function (data) {
   console.log("itemback:", data.phrase);
   var elements = document.getElementsByClassName("gentext")[0];
   elements.innerHTML = data.phrase;
-  // elements[0].className += " clicked";
 });
-
-// function clickgo() {
-// 	var elements = document.getElementsByClassName(this.className);
-// 	socket.emit('item', elements[0].className.split(" ")[1]);
-// 	elements[0].className += " clicked";
-// 	elements[0].style.backgroundColor = user.color;
-// }
-
 
 socket.on('audienceEnable', function (data) {
   console.log('enabled? ', data);
@@ -190,6 +105,96 @@ socket.on('triggerEnding', function (data) {
   dSound.playEnding();
 });
 
+/**********************************************
+FUNCTIONS
+**********************************************/
+
+function actOnText() {
+  var contents = $('.scoreText').text().split(" "),
+    modText = '';
+
+  for (var i = 0; i < contents.length; i++) {
+    modText += '<span>' + contents[i] + '</span> ';
+  }
+
+  $('.scoreText').html(modText);
+
+}
+
+function createUL(array) {
+  let str = '<ul>'
+
+  array.forEach(function (slide) {
+    str += '<li>' + slide + '</li>';
+  });
+
+  str += '</ul>';
+
+  let myEle = document.getElementById("session-list");
+  if (myEle) {
+    document.getElementById("session-list").innerHTML = str;
+  }
+}
+
+function createOpts(array) {
+  // let sel = document.getElementById("sessionSelect");
+  // let sessionName = selectEle.innerHTML;
+
+  let audText = '<option name="session-name" value="/audience">audience</option><option disabled>Choose a session:</option>';
+
+  let sel = document.getElementById('sessionSelect');
+  if (sel) {
+    // let sessionName = sel.options[sel.selectedIndex].text;
+    let sessionName = sel.innerHTML;
+
+    array.forEach(function (item) {
+      sessionName += '<option name="session-name" value="/audience">' + item + '</option>';
+    });
+    // console.log("sel.innerHTML", sel.innerHTML);
+    sel.innerHTML = audText + sessionName;
+  }
+}
+
+function registerWithServer() {
+  socket.emit('addme', {
+    name: user.name,
+    color: user.color
+  });
+}
+
+function registerPoet(corpusName = 'horrortech-corpus') {
+  // let corpusName = 'horrortech-corpus';
+  let sessionName = document.forms["poet-form"]["session-name"].value;
+  // let corpusName = document.forms["poet-form"]["corpus-name"].value;
+  console.log("Session Name: ", sessionName);
+  console.log("Corpus Name: ", corpusName);
+
+  socket.emit('registerSession', {
+    'name': sessionName,
+    'corpus': corpusName,
+    // 'corpus': 'horrortech-corpus',
+    'date': Date.now()
+  });
+  localStorage.setItem("sessionName", sessionName);
+  user.sessionName = sessionName;
+  return false;
+};
+
+function registerAudience(sessionName) {
+  // let sessionName = document.forms["audience-form"]["session-name"].value;
+  // console.log("Session Name: ", sessionName);
+  socket.emit('joinSession', {
+    'name': sessionName,
+    'user': user.socketID
+  });
+  localStorage.setItem("sessionName", sessionName);
+  user.sessionName = sessionName;
+  return false;
+};
+
+function getSessions() {
+  socket.emit('getSessions', 'please');
+}
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF'.split('');
@@ -200,31 +205,20 @@ function getRandomColor() {
   return color;
 }
 
+/**********************************************
+EVENTS
 
-// Add Chords, Progression, feedback, comb filtering, shimmer!
+Let's try to keep events out of here and move them over to the individual .html files that they belong to.
+**********************************************/
 
-var self = this;
+// $('.scoreText').click(function (e) {
+//   target = event.target || event.srcElement;
 
-var speakVoice = 'en/en';
-meSpeak.loadConfig("/js/mespeak/mespeak_config.json");
-//meSpeak.loadVoice('js/mespeak/voices/en/en-us.json');
-meSpeak.loadVoice('/js/mespeak/voices/' + speakVoice + '.json');
-//meSpeak.speak('hello world',{},toneSetup());
+//   if (target.nodeName.toLowerCase() === "span") {
+//     var text = $(e.target).text();
 
-var dSound = new DiamondSound();
-
-function createUL(array) {
-  var str = '<ul>'
-
-  array.forEach(function (slide) {
-    str += '<li>' + slide + '</li>';
-  });
-
-  str += '</ul>';
-
-  var myEle = document.getElementById("session-list");
-  if (myEle) {
-    document.getElementById("session-list").innerHTML = str;
-  }
-}
-
+//     $(e.target)[0].style.backgroundColor = user.color;
+//     dSound.speak(text);
+//     socket.emit('item', text);
+//   }
+// });
